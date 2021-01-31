3bf60b284d0e6dac5c472f6d1e5a85644b94885e
Haitham Alqadamani <haqad66@gmail.com>
2021-01-31 22:58:38 +1100

    Lesson4 , Create Computer MVC

---
+++ IntroductionToMVC/Controllers/ComputerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IntroductionToMVC.Models;
+
+namespace IntroductionToMVC.Controllers
+{
+    public class ComputerController : Controller
+    {
+       public ActionResult Display()
+        {
+            var MyComputer = new Computer
+            {
+                Name = "Lenovo ThinkPad",
+                Price = 500
+            };
+            return View(MyComputer);
+        }
+    }
+}
---
+++ IntroductionToMVC/Models/Computer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroductionToMVC.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public double Price { get; set; }
+    }
+}
