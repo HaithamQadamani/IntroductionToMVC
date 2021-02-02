1a1bb981da7486440f3899837e48621b5925f706
Haitham Alqadamani <haqad66@gmail.com>
2021-02-02 21:35:57 +1100

    Lesson8, Razor MVC

---
+++ MVC From Scratch/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using MVC_From_Scratch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_From_Scratch.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult DisplayCustomer()
+        {
+            var customer = new Customer(1,"Haitham", "Amman", 20);
+            return View(customer);
+        }
+    }
+}
---
+++ MVC From Scratch/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_From_Scratch.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+
+        public Customer (int id, string name, string address, int age)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            Age = age;
+        }
+    }
+}
